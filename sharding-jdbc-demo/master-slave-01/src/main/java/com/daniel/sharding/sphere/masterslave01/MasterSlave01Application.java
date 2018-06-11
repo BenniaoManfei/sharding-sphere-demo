bbe2c96535a946dfe36247a33f81208c95482385
daizm <daizm@bangmart.cc>
2018-06-11 17:46:12 +0800

    添加README

--- sharding-jdbc-demo/master-slave-01/src/main/java/com/daniel/sharding/sphere/masterslave01/MasterSlave01Application.java
+++ sharding-jdbc-demo/master-slave-01/src/main/java/com/daniel/sharding/sphere/masterslave01/MasterSlave01Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MasterSlave01Application {
 
 	public static void main(String[] args) {
+		System.err.println();
 		SpringApplication.run(MasterSlave01Application.class, args);
 	}
 }
